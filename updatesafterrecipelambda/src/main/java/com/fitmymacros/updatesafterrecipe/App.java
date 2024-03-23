ba0fbc72dd53753327394fa6cbf9da522eb6a293
Pablo <pabloskixd23@gmail.com>
2024-03-23 15:13:20 +0100

    maven project generation

---
+++ updatesafterrecipelambda/src/main/java/com/fitmymacros/updatesafterrecipe/App.java
@@ -0,0 +1,13 @@
+package com.fitmymacros.updatesafterrecipe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
